dac9cf654a6a57a619d68311b3e58d7eb582ca21
romeucamurca <romeucamurcarabelo@gmail.com>
2020-08-21 21:36:01 -0300

    add correção modal deletar ficha e mascaras users

--- src/main/resources/static/js/jquery-mascaras.js
+++ src/main/resources/static/js/jquery-mascaras.js
@@ -25,29 +25,5 @@ $(document).ready(
 					thousands : "."
 				});
 			});
-			
-			//mascara do campo telefone do modal de adicionar usuário
-			var behavior = function(val) {
-				return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000'
-						: '(00) 0000-00009';
-			}, options = {
-				onKeyPress : function(val, e, field, options) {
-					field.mask(behavior.apply({}, arguments), options);
-				}
-			};
-			
-			$('#telefone_add_user').mask(behavior, options);
-			
-			//mascara do campo telefone do modal de atualizar usuário
-			var behavior = function(val) {
-				return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000'
-						: '(00) 0000-00009';
-			}, options = {
-				onKeyPress : function(val, e, field, options) {
-					field.mask(behavior.apply({}, arguments), options);
-				}
-			};
-			
-			$('#telefone_edit').mask(behavior, options);
 
 		});
